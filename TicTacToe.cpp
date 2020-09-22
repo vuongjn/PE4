576686e7d7b83393c9a7052e41c26e0472c74bf7
Justin Vuong <jnvuong26@gmail.com>
2020-09-22 17:11:53 -0600

    Created DisplayBoard Function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -14,4 +14,13 @@ class TicTacToe
             }
         }
     }
+
+    void DisplayBoard(){
+        for(int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                std::cout << board_[i][j];
+            }
+            std::cout << std::endl;
+        }
+    }
 };
